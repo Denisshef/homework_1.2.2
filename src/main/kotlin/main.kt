fc3e8e86abafd5a2259b0664443397f3f0d0a239
Denisshef <denisshef24@yandex.ru>
2022-03-15 19:35:06 +0700

    Edit main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,12 +4,14 @@ fun main() {
     printLikes(11)
     printLikes(21)
     printLikes(56)
+    printLikes(31)
+    printLikes(1111)
 }
 
 fun printLikes(likes: Int) {
     if (likes == 0) {
         println("Никому не понравилось ((")
-    } else if (likes == 11 || likes == 111) {
+    } else if (likes % 100 == 11) {
         println("Понравилось $likes людям")
     } else if (likes == 1 || ((likes % 10) == 1)) {
         println("Понравилось $likes человеку")
